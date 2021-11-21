8be60d8359bf08b9e71f1f78a7ff79d57f021639
David Teixeira <d.teixeira@whisbi.com>
2021-11-21 22:16:09 +0100

    deploy test

--- src/index.ts
+++ src/index.ts
@@ -24,7 +24,8 @@ function initializeExpressServer() {
     });
   });
 
-  app.listen(process.env.PORT || 3000);
+  const port = process.env.PORT || 3000;
+  app.listen(port);
 
   console.log("Express server has started on port 3000. Open http://localhost:3000 to see results");
 }
